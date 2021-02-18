4ec490d0696fee29623e875f5a1d4392e3da2465
Philippe Schommers <philippe@schommers.be>
2021-02-18 16:45:21 +0100

    Add support for gBZZ

--- utils/tokens.js
+++ utils/tokens.js
@@ -4,7 +4,14 @@ module.exports = {
     bokky: {
       decimals: 18,
       address: '0x583cbBb8a8443B38aBcC0c956beCe47340ea1367',
-      name: 'BokkyPooBah Test Token'
-    }
-  }
+      name: 'BokkyPooBah Test Token',
+    },
+  },
+  goerli: {
+    gbzz: {
+      decimals: 16,
+      address: '0x2ac3c1d3e24b45c6c310534bc2dd84b5ed576335',
+      name: 'gBZZ',
+    },
+  },
 }
