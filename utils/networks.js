ece3f9acb18882c8fb372aad5bf286d49c4bcf04
Philippe Schommers <philippe@schommers.be>
2021-02-18 16:45:15 +0100

    Add support for Goerli

--- utils/networks.js
+++ utils/networks.js
@@ -4,10 +4,20 @@ module.exports = {
     provider: 'https://ropsten.infura.io/v3/a920c260dd3145c5a3770e182c3333d8',
     chainId: 3,
     etherscan: {
-      getTxUrl: tx => {
+      getTxUrl: (tx) => {
         return `https://ropsten.etherscan.io/tx/${tx}`
-      }
-    }
+      },
+    },
+  },
+  goerli: {
+    name: 'goerli',
+    provider: 'https://goerli.infura.io/v3/a920c260dd3145c5a3770e182c3333d8',
+    chainId: 5,
+    etherscan: {
+      getTxUrl: (tx) => {
+        return `https://goerli.etherscan.io/tx/${tx}`
+      },
+    },
   },
   mainnet: {
     name: 'mainnet',
@@ -15,9 +25,9 @@ module.exports = {
     // provider: 'https://mainnet.infura.io/v3/a920c260dd3145c5a3770e182c3333d8',
     chainId: 1,
     etherscan: {
-      getTxUrl: tx => {
+      getTxUrl: (tx) => {
         return `https://etherscan.io/tx/${tx}`
-      }
-    }
-  }
+      },
+    },
+  },
 }
